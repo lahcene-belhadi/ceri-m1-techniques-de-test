fb20c0cfae71a5ddeaf03ca099589ea4ba643e87
Lahcène Belhadi <lahcene.belhadi@alumni.univ-avignon.fr>
2023-04-04 16:59:42 +0200

    Fin TP3

---
+++ src/test/fr.univavignon.pokedex.api/PokemonTrainerFactoryTests.java
@@ -0,0 +1,26 @@
+import main.java.fr.univavignon.pokedex.api.*;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Assert;
+
+import org.mockito.Mockito;
+
+public class PokemonTrainerFactoryTests {
+    private PokemonTrainer pokemonTrainer;
+
+    @Before
+    public void init() throws Exception {
+        IPokedexFactory pokedexFactory = Mockito.mock(IPokedexFactory.class);
+        IPokedex pokedex = Mockito.mock(IPokedex.class);
+        IPokemonTrainerFactory pokemonTrainerFactory = Mockito.mock(IPokemonTrainerFactory.class);
+
+        Mockito.when(pokemonTrainerFactory.createTrainer("Trainer", Team.VALOR, pokedexFactory)).thenReturn(new PokemonTrainer("Trainer", Team.VALOR, pokedex));
+
+        pokemonTrainer = pokemonTrainerFactory.createTrainer("Trainer", Team.VALOR, pokedexFactory);
+    }
+
+    @Test
+    public void okIfTrainerNameIsTrainer() {
+        Assert.assertTrue(pokemonTrainer.getName() == "Trainer");
+    }
+}
