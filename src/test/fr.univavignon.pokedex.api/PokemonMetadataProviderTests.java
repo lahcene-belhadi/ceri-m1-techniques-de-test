4d0cdf790c078a0b64efc0bd4af85246be56b9ec
Lahcène Belhadi <lahcene.belhadi@alumni.univ-avignon.fr>
2023-04-04 16:06:18 +0200

    Adds PokemonMetadataProviderTests

---
+++ src/test/fr.univavignon.pokedex.api/PokemonMetadataProviderTests.java
@@ -0,0 +1,25 @@
+import main.java.fr.univavignon.pokedex.api.IPokemonMetadataProvider;
+import main.java.fr.univavignon.pokedex.api.PokemonMetadata;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.Mockito;
+
+
+public class PokemonMetadataProviderTests {
+    private PokemonMetadata bulbizarre;
+
+    @Before
+    public void init() throws Exception {
+        IPokemonMetadataProvider pokemonMetadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+        Mockito.when(pokemonMetadataProvider.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
+        bulbizarre = pokemonMetadataProvider.getPokemonMetadata(0);
+    }
+
+    @Test
+    public void okIfBulbizarreIsNotNull() {
+        Assert.assertTrue(bulbizarre != null);
+    }
+}
